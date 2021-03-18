398c3a028eb28d8f59a252423dd8db3b57e5082d
xkendx <63884610+xkendx@users.noreply.github.com>
2021-03-18 04:06:14 +0300

    feat: create project skeleton

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: 'AIzaSyA20x5Ij21cOQC9WAnJ46fV7_5LpwEiWMQ',
+    authDomain: 'messenger-3b6b7.firebaseapp.com',
+    projectId: 'messenger-3b6b7',
+    storageBucket: 'messenger-3b6b7.appspot.com',
+    messagingSenderId: '972694073694',
+    appId: '1:972694073694:web:5a71fc2b8bc58769695fcf',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
