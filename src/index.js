3d193da228340db80cc498e022b88af20f4c9e2f
xkendx <63884610+xkendx@users.noreply.github.com>
2021-03-24 03:42:29 +0300

    fix: dotenv

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ import store from './store';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyBOnM-bt3sMS2SAkKriPjrbLv_XJlK3XUA',
-    authDomain: 'claruswaychat-e0717.firebaseapp.com',
-    projectId: 'claruswaychat-e0717',
-    storageBucket: 'claruswaychat-e0717.appspot.com',
-    messagingSenderId: '384849413735',
-    appId: '1:384849413735:web:2d0aba3bd27a213b4536c2',
-    // measurementId: "G-VXYE76S2G7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
